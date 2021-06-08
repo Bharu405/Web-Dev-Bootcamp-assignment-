fcfc687af90b8704983557c81afcb1e1bb7ed940
Bharu405 <85537201+Bharu405@users.noreply.github.com>
2021-06-08 19:40:33 +0530

    Add files via upload

---
+++ microsoft bootcamp-day1-assignment/afternoon --ass-prog2.js
@@ -0,0 +1,3 @@
+function changecolor() {
+	document.getElementById("123").style.color="red"
+        } 
---
+++ microsoft bootcamp-day1-assignment/assignment--prog4.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>external script button</title>
+</head>
+<body>
+<script src="changecolor.js"></script>
+<p id="123">the color of text will change to red by clicking below button</p>
+<button type ="button" onclick="changecolor()">click to change color</button>
+</body>
+</html> 
